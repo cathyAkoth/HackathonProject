70a13899d16f9d32a162f2c6f711d429493d5819
CathyAkoth <catherinececilia22@gmail.com>
2021-11-09 20:27:28 -0800

    Successful connect to MongoDB atlas

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const express = require("express");
+const express = require('express');
 require('dotenv').config()
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 
+
+
+const { PORT } = process.env || 5000 
+const { WELCOME_MESSAGE, DATABASE_URL } = process.env
+
+
+// declare app isntance
 const app = express();
-app.use(express.json())
 
-const port = 5000;
-const url = process.env.MONGODB_CONNECTION_STRING;
+// load middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true }))
 
-mongoose.connect(uri,{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology:true,
-});
 
-const connection = mongoose.connection;
-connection.once("open", () => {
-    console.log("MongoDB database connection established successfully");
+
+// spin up the server 
+mongoose.connect(DATABASE_URL).then(() => {
+    // successful connection
+    app.listen(PORT, ()=> {
+        let message = `${WELCOME_MESSAGE} http://localhost:${PORT}`
+        console.log(message)
+    })
+}).catch(error => {
+    console.error("Failed to start the server due to : ",error)
 })
-
-app.listen(5000, () => console.log('listening on port 5000'));
-
-
