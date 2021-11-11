bffcb19f116ddb395f7bdbb4d0fed391b8760a83
CathyAkoth <catherinececilia22@gmail.com>
2021-11-10 17:47:32 -0800

    student form posting to DB

--- Models/StudentForm.js
+++ Models/StudentForm.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // Create a schema for data that is needed to be saved.
 const studentFormSchema = new mongoose.Schema({
-  firstName: {
+    firstName: {
     type: String,
     //required: 'please fill in',
   },
@@ -9,11 +9,11 @@ const studentFormSchema = new mongoose.Schema({
     type: String,
     //required: 'please fill in',
   },
-  dob:{
-      type: String,
-      //required:'please select',
-  },
-  residence:{
+  email:{
+    type: String,
+    //required:'please select',
+},
+  phoneNumber:{
     type: String,
     //required: 'please enter service'
   },
@@ -21,15 +21,15 @@ const studentFormSchema = new mongoose.Schema({
     type: String,
     //required: 'please fill in',
   },
- nationality:{
+ country:{
       type: String,
       //required: 'please fill in',
   },
-  gender:{
+  course:{
     type: String,
     //required:'please select',
   },
-  category:{
+  status:{
   type: String,
   //require: 'please enter '
   },
